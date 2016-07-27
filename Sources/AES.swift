d486fb3c51ec1c16e318daff6f6f84f7fbd35b7a
Khoa Pham <onmyway133@gmail.com>
2016-07-27 14:22:47 +0200

    Add AES

---
+++ Sources/AES.swift
@@ -0,0 +1,14 @@
+//
+//  AES.swift
+//  CommonCryptoSwift
+//
+//  Created by Khoa Pham on 27/07/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import Foundation
+
+public struct AES {
+
+  
+}
