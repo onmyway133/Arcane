cedc4433de108c01b2c0686bbd0e0ab1d214d7a8
Khoa Pham <onmyway133@gmail.com>
2017-11-14 10:30:36 +0100

    Add tvOS and watchOS targets

---
+++ Arcane-tvOS/Arcane_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Arcane_tvOS.h
+//  Arcane-tvOS
+//
+//  Created by Khoa Pham on 14.11.2017.
+//  Copyright © 2017 Fantageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Arcane_tvOS.
+FOUNDATION_EXPORT double Arcane_tvOSVersionNumber;
+
+//! Project version string for Arcane_tvOS.
+FOUNDATION_EXPORT const unsigned char Arcane_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arcane_tvOS/PublicHeader.h>
+
+
